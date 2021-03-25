c79cf0633e171dbb7bfcd56e5b4c902d5d336c58
POPS\huy.thanh <huy.thanh@pops.vn>
2021-03-25 16:27:46 +0700

    update unlock comic v2

---
+++ POPSTestAutomation/Object Repository/ComicPageUI/COMICDETAILPAGEUI/BUTTON_BUY_NOW.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>BUTTON_BUY_NOW</name>
+   <tag></tag>
+   <elementGuidId>44a06130-81a4-474a-8a27-86bed73b5404</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@class='button-regular button-primary comic__modal__btn--unlock button-large button-primary']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ POPSTestAutomation/Object Repository/ComicPageUI/COMICDETAILPAGEUI/BUTTON_SORT.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>BUTTON_SORT</name>
+   <tag></tag>
+   <elementGuidId>06869415-79ed-4d25-afbd-9f0e897fe314</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@class='button-regular button-primary button-sort']/span[@class='button-icon']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
