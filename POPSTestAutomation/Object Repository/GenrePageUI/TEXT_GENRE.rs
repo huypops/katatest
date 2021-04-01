0e3b35839d8c340b92794960cbc54779513fd1fd
POPS\huy.thanh <huy.thanh@pops.vn>
2021-04-01 10:07:51 +0700

    deleted old folder project

--- POPSTestAutomation/Object Repository/GenrePageUI/TEXT_GENRE.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>TEXT_GENRE</name>
-   <tag></tag>
-   <elementGuidId>86b34d28-341a-4913-8369-dd5020dd32e4</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
